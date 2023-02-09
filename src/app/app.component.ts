888aba3ee9006942f8f04feeeb221d61013a9a2c
Rajeev Ranjan Srivastava <rajeevsrivastava468@gmail.com>
2023-02-09 08:13:03 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,62 @@ import { Component} from '@angular/core';
 })
 export class AppComponent {
 
+
+  searchMoviesCtrl = new FormControl();
+  filteredMovies: any;
+  isLoading = false;
+  errorMsg!: string;
+  minLengthTerm = 3;
+  selectedMovie: any = "";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  onSelected() {
+    console.log(this.selectedMovie);
+    this.selectedMovie = this.selectedMovie;
+  }
+
+  displayWith(value: any) {
+    return value?.Title;
+  }
+
+  clearSelection() {
+    this.selectedMovie = "";
+    this.filteredMovies = [];
+  }
+
+  ngOnInit() {
+    this.searchMoviesCtrl.valueChanges
+      .pipe(
+        filter(res => {
+          return res !== null && res.length >= this.minLengthTerm
+        }),
+        distinctUntilChanged(),
+        debounceTime(1000),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredMovies = [];
+          this.isLoading = true;
+        }),
+        switchMap(value => this.http.get('http://www.omdbapi.com/?apikey=' + API_KEY + '&s=' + value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+        )
+      )
+      .subscribe((data: any) => {
+        if (data['Search'] == undefined) {
+          this.errorMsg = data['Error'];
+          this.filteredMovies = [];
+        } else {
+          this.errorMsg = "";
+          this.filteredMovies = data['Search'];
+        }
+        console.log(this.filteredMovies);
+      });
+  }
 }
